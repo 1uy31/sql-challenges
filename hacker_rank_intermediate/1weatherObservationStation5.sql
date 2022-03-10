3d4519010a70c12d961197c08510026387010c46
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-03-10 23:11:16 +0200

    All selection first

--- hacker_rank_intermediate/1weatherObservationStation5.sql
+++ hacker_rank_intermediate/1weatherObservationStation5.sql
@@ -1,6 +1,6 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
 WITH cityAndItsNameLength as
-    (SELECT CITY as City FROM STATION, LENGTH(CITY) as CityNameLength)
+    (SELECT CITY as City, LENGTH(CITY) as CityNameLength FROM STATION)
     (SELECT City, CityNameLength FROM cityAndItsNameLength ORDER BY CityNameLength ASC, City ASC LIMIT 1)
     UNION
     (SELECT City, CityNameLength FROM cityAndItsNameLength ORDER BY CityNameLength DESC, City ASC LIMIT 1);
