cc877d98c6c753dc40ed30dc1564107264b8a3ed
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-03-10 22:47:33 +0200

    The report

---
+++ hacker_rank_intermediate/4theReport.sql
@@ -0,0 +1,13 @@
+-- https://www.hackerrank.com/challenges/the-report/problem?isFullScreen=true
+SELECT
+CASE
+    WHEN g.Grade >= 8 THEN s.Name
+    ELSE null  -- If Grade < 8, use null as Name
+END as Name, g.Grade, s.Marks
+FROM Students s
+LEFT JOIN Grades g ON s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY g.Grade DESC,
+(CASE
+    WHEN g.Grade >= 8 THEN Name
+    ELSE s.Marks  -- If Grade < 8, order by Grade and Marks
+END) ASC;
